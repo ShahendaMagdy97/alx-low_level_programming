01c612f6ee992073a2a713ad9b90b522dc1fafac
shahenda magdy <shahendamagdy97@yahoo.com>
2023-03-21 04:22:31 -0700

    b

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - entry point
  *
- * Description: print the value of n status
+ * Description: print the value of n status:
  *		greater than, is zero and is not less than 6.
  *
  * Return: 0 (success)
@@ -13,7 +13,7 @@
 
 int main(void)
 {
-	int n digit;
+	int n, digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
