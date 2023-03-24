0fe505d98fbe0f5ba00793ec0ca47cbaadd013dd
shahenda magdy <shahendamagdy97@yahoo.com>
2023-03-24 06:28:33 -0700

    c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,17 +9,17 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int x, y;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (postn = 1; postn <= n; postn++)
+		for (x = 1; x <= n; x++)
 		{
-			for (space = 1; space <= postn; space++)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
+			for (y = 1; y <= x; y++)
+				_putchar(32);
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
