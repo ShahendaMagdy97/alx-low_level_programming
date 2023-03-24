87fd6563c149990ea582e7046071c643caa43528
shahenda magdy <shahendamagdy97@yahoo.com>
2023-03-24 16:54:41 -0700

    c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,21 +17,25 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && num % 5 != 0)
+		if (num % 3 == 0)
 		{
 			printf("Fizz");
-		} else if (num % 5 == 0 && num % 3 != 0)
+		}
+		else if (num % 5 == 0)
 		{
 			printf("Buzz");
-		} else if (num % 3 == 0 && num % 5 == 0)
+		}
+		else if ((num % 3 == 0) && (num % 5 == 0))
 		{
 			printf("FizzBuzz");
-		} else if (num == 1)
+		}
+		else
 		{
 			printf("%d", num);
-		} else
+		}
+		if (num != 100)
 		{
-			printf(" %d", num);
+			printf(" ");
 		}
 	}
 	printf("\n");
