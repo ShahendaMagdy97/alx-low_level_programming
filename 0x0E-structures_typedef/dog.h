f8009b20903e61e79bd29419661317dc0acec3bd
shahenda magdy <shahendamagdy97@yahoo.com>
2023-04-17 08:09:05 -0700

    zero

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @ownew: owner's name
+ *
+ * Description: a poppy dog struct in a kitty world
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
