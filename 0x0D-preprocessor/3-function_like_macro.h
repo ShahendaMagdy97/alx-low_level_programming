0d8639e40a0ed8edd9fd05ce86edca3d54d16394
shahenda magdy <shahendamagdy97@yahoo.com>
2023-04-17 07:07:07 -0700

    third

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
-#include <stdio.h>
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
 
-/**
- * main - print name of this source file
- *
- * Return: Always 0
-*/
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-int main(void)
-{
-	printf("%s\n", _FILE_);
-	return (0);
-}
+#endif /*_FUNCTION_LIKE_MACRO */
