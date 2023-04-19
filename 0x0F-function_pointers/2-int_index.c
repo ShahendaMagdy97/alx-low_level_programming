bf7f9a8149ea5889eb77100ff949a87555a93f85
shahenda magdy <shahendamagdy97@yahoo.com>
2023-04-19 20:56:01 +0200

    Edited Third Commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,12 +12,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x = 0;
 
-	if (array && size && cmp)
-		while (x < size)
-		{
-			if (cmp(array[x]))
-				return (x);
-				x++;
-		}
-		return (-1);
+		if (array && size && cmp)
+			while (x < size)
+			{
+				if (cmp(array[x]))
+					return (x);
+			x++;
+			}
+	return (-1);
 }
