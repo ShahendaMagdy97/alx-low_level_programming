0c6aaaf480804b5bd8b59403aca827a7f4d75a43
shahenda magdy <shahendamagdy97@yahoo.com>
2023-04-28 21:49:09 +0200

    Edited Third Commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,30 +1,36 @@
 #include "lists.h"
 
 /**
- * add_node - adds a node to the start of the list
+ * add_node_end - adds a node to the end of the list
  * @padd: address of pointer to head node
  * @strn: str field of node
  *
  * Return: size of list
  */
-list_t *add_node(list_t **padd, const char *strn)
+list_t *add_node_end(list_t **padd, const char *strn)
 {
-	list_t *new_head = malloc(sizeof(list_t));
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *node = *padd;
 
-	if (!padd || !new_head)
+	if (!padd || !new_node)
 		return (NULL);
 	if (strn)
 	{
-		new_head->str = strdup(strn);
-		if (!new_head->str)
+		new_node->str = strdup(strn);
+		if (!new_node->str)
 		{
-			free(new_head);
+			free(new_node);
 			return (NULL);
 		}
-		new_head->len = _strlen(new_head->str);
+		new_node->len = _strlen(new_node->str);
 	}
-
-	new_head->next = *padd;
-	*padd = new_head;
-	return (new_head);
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*padd = new_node;
+	return (new_node);
 }
